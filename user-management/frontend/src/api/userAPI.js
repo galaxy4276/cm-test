e536be65e47a5595da110542d4786874da3cc157
galaxy4276 <galaxyhi4276@gmail.com>
2021-02-16 13:49:15 +0900

    feat: 프런트엔드 API 작성

---
+++ user-management/frontend/src/api/userAPI.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'http://localhost:8001'
+});
+
+export const addUserToJSON = (user) => {
+  return client.post('/user', user);
+};
+
+export const getUserFromJSON = () => {
+  return client.get('/user');
+};
